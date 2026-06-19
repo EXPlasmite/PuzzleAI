45a3c03185dd5634e7792c0ae0dc54cba0c07d92
EXPlasmite <ty.bingham1@outlook.com>
2026-06-19 02:59:41 +0100

    ML Agents installation and basic setup

---
+++ Assets/Scripts/PushAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushAgent : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
